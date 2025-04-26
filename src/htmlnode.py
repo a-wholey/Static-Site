f904e516be94e8c5aa8154ba6b29af6be1e2630d
a-wholey <alexandriawholey@gmail.com>
2025-04-25 22:37:05 -0400

    Create ParentNode in HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,3 +30,13 @@ class LeafNode(HTMLNode):
             return self.value
         props_str = self.props_to_html()
         return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("parent nodes must have tag")
+        if self.value is None:
+            raise ValueError("parent nodes must have value")
